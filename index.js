4e358253e03ec8be56403f240ffc35a2b57530ea
jobo322 <jose.bolanos@correounivalle.edu.co>
2022-07-12 08:21:40 -0500

    feat!: use a closure to have two methods

--- index.js
+++ index.js
@@ -4,6 +4,26 @@ const { fileListFromPath } = require("filelist-utils");
 
 const path = join(__dirname, "./data/");
 
+function jeol() {
+  const fileList = fileListFromPath(path);
+
+  const list = () => fileList.map((d) => d.name);
+
+  const get = async (names) => {
+    const arrayNames = !Array.isArray(names) ? [names] : names;
+    const result = [];
+    for (let name of arrayNames) {
+      result.push(
+        ...fileList
+        .filter((d) => d.name === name)
+      )
+    }
+    return result;
+  }
+
+  return { list, get, fileList };
+}
+
 module.exports = {
-  jeol: () => fileListFromPath(path),
+  jeol
 };
