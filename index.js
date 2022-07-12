941fc13587e3dd8e6d7f4366ab414e04cf613815
jobo322 <jose.bolanos@correounivalle.edu.co>
2022-07-12 07:41:41 -0500

    chore: use arrow functions

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const { join } = require("path");
 
 const { fileListFromPath } = require("filelist-utils");
 
-function getData() {
-  return fileListFromPath(join(__dirname, "./data/"));
-}
+const path = join(__dirname, "./data/");
+
 module.exports = {
-  getData,
+  jeol: () => fileListFromPath(path),
 };
