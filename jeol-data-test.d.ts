448a798b2a42cd80b9d5a9a865968b96b66625d6
jobo322 <jose.bolanos@correounivalle.edu.co>
2022-08-30 14:46:19 -0500

    feat!: add more specific functions

--- jeol-data-test.d.ts
+++ jeol-data-test.d.ts
@@ -2,6 +2,8 @@ import { PartialFile } from 'filelist-utils';
 
 export function getList(): Promise<string[]>;
 
-export function getFileList(name: string): Promise<PartialFile>;
+export function getFile(name: string): Promise<PartialFile>;
+
+export function getFileList(name: string): Promise<PartialFileList>;
 
 export function getData(name: string): Promise<ArrayBuffer>;
