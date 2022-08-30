6a66797efe1e7376bc92bd435081a47ad65381c6
jobo322 <jose.bolanos@correounivalle.edu.co>
2022-08-30 08:41:27 -0500

    feat!: async functions

--- src/__tests__/simple.test.js
+++ src/__tests__/simple.test.js
@@ -1,8 +1,8 @@
 import { getList } from '../index';
 
 describe('simple test', () => {
-  it('check one name', () => {
-    const list = getList();
+  it('check one name', async () => {
+    const list = await getList();
 
     expect(list).toContain(
       '8PA_SynLK_5360u150uDMSO_snc1811_qH_spinOff-1-2.jdf',
--- src/index.js
+++ src/index.js
@@ -3,13 +3,29 @@ import { join } from 'path';
 import { fileListFromPath } from 'filelist-utils';
 
 const path = join(__dirname, '../data/');
-const fileList = fileListFromPath(path);
 
-export function getList() {
+const cache = {};
+
+async function loadFileList(path) {
+  if (cache[path]) {
+    return cache[path];
+  }
+
+  cache[path] = await fileListFromPath(path, {
+    unzip: { zipExtensions: [] },
+    ungzip: { gzipExtensions: [] },
+  });
+
+  return cache[path];
+}
+
+export async function getList() {
+  const fileList = await loadFileList(path);
   return fileList.map((d) => d.name);
 }
 
-export function getFileList(name) {
+export async function getFileList(name) {
+  const fileList = await loadFileList(path);
   const result = fileList.filter((d) => d.name === name);
 
   if (result.length < 1) {
